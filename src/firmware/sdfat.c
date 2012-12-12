5f3ad4f1f21af648050a4326d72b6ea7d3921407
Nathan Dumont <n.dumont@bath.ac.uk>
2012-12-12 09:03:41 +0000

    Started re-licensing as LGPLv3 to allow re-use of parts in closed products

--- src/firmware/sdfat.c
+++ src/firmware/sdfat.c
@@ -4,7 +4,7 @@
  * Copyright Nathan Dumont 2012 <nathan@nathandumont.com>
  *
  * This firmware is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
